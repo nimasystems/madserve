a4dd2770c893922d52686c5c02b14db81b20a736
mkovachev <mkovachev@2f0e05dc-1968-4751-bacf-4d00df7d842d>
2014-02-17 17:36:04 +0000

    Changes: grabo

--- trunk/upload/modules/network_modules/GRABO/request.php
+++ trunk/upload/modules/network_modules/GRABO/request.php
@@ -5,8 +5,10 @@ class GRABO implements networkmodule {
 		error_reporting(0);
 			
 		global $zone_detail;
-			
-		$httpConfig['method']     = 'POST';
+
+		$request_url = 'http://b.grabo.bg/mobile/_nimasystems/popup';
+
+		$httpConfig['method']     = 'GET';
 		$httpConfig['timeout']    = 1;
 		$httpConfig['siteid']     = $network_ids['p_1'];
 			
@@ -25,8 +27,9 @@ class GRABO implements networkmodule {
 			//$http->addParam('p_carrier'   , $request_info['ip_address']);
 			//$http->addParam('p_longtitude'   , $request_info['longitude']);
 			//$http->addParam('p_latitude'   , $request_info['latitude']);
-				
+
 			$http->addParam('p_siteid'   , $network_ids['p_1']);
+			$http->addParam('affid'   , $network_ids['p_1']);
 			$http->addParam('p_user_agent'   , $request_info['user_agent']);
 			$http->addParam('p_page_url'   , $request_info['referer']);
 			$http->addParam('p_main_device', $request_info['main_device']);
@@ -42,7 +45,7 @@ class GRABO implements networkmodule {
 			return false;
 		}
 
-		/*$http->execute($request_url);
+		$http->execute($request_url);
 
 		if ($http->error){
 		return false;
@@ -52,64 +55,16 @@ class GRABO implements networkmodule {
 		return false;
 		}
 
-		try {
-		$xml_response = new SimpleXmlElement($http->result, LIBXML_NOCDATA);
-		} catch (Exception $e) {
-		// handle the error
-		return false;
-		}*/
-
 		if ($request_type == 'banner') {
-			$tempad['url'] = 'http://b.grabo.bg/mobile/_nimasystems/popup/?affid=18850'; // 'http://b.grabo.bg/?city=&affid=18825&size=300x600';
+
+			$tempad['url'] = 'http://b.grabo.bg/mobile/_nimasystems/popup/?affid=18850';
 			$tempad['type'] = 'banner';
-			/*$tempad['markup'] = (rand(1, 2) == 2 ?
-					'<html><body style="height:100%;background-color:green">this is a <b style="color:red">test</b> string</body></html>' :
-					'<html><body style="height:100%;background-color:green">HOORAYY!</body></html>'
-			);*/
-
-			$tempad['markup'] = file_get_contents('http://b.grabo.bg/mobile/_nimasystems/footer/?affid=18850');
-			//$tempad['markup'] = file_get_contents('http://ads.nimasystems.com/data/creative/grabo/banner_hx50.html');
-
-			/*<?xml version="1.0" encoding="UTF-8" ?>
-			 <request type="textAd"><htmlString skipoverlaybutton="0"><![CDATA[<html><body>HOORAYY!</body></html>]]></htmlString><clicktype>inapp</clicktype><clickurl><![CDATA[http://madserve.localhost/md.click.php?zone_id=3&h=1a1e8dcabe648ae330b28b919838edac&type=network&campaign_id=1&network_id=32&c=aHR0cDovL3d3dy5uaW1hc3lzdGVtcy5jb20,]]></clickurl><urltype>link</urltype><refresh>30</refresh><scale>no</scale><skippreflight>yes</skippreflight></request>*/
-
-			// http://madserve.localhost/md.request.php?sdk=banner&c.mraid=1&o_iosadvidlimit=0&rt=iphone_app&u=Mozilla%2F5.0%20%28iPhone%3B%20CPU%20iPhone%20OS%207_1%20like%20Mac%20OS%20X%29%20AppleWebKit%2F537.51.2%20%28KHTML%2C%20like%20Gecko%29%20Mobile%2F11D5145e&u_wv=Mozilla%2F5.0%20%28iPhone%3B%20CPU%20iPhone%20OS%207_1%20like%20Mac%20OS%20X%29%20AppleWebKit%2F537.51.2%20%28KHTML%2C%20like%20Gecko%29%20Mobile%2F11D5145e&u_br=Mozilla%2F5.0%20%28iPhone%3B%20CPU%20iPhone%20OS%207_1%20like%20Mac%20OS%20X%29%20AppleWebKit%2F537.51.2%20%28KHTML%2C%20like%20Gecko%29%20Version%2Funknown%20Mobile%2F11D5145e%20Safari%2Funknown&o_iosadvid=92FCD837-4F8E-4B58-9158-30E390D08C26&v=4.1.6&s=5cfbb96276738ca82edd04225c3b5c1d&iphone_osversion=7.1&spot_id=
-
-
+			$tempad['markup'] = $http->result;
 		} else {
 			$tempad['type'] = 'banner';
 			$tempad['interstitial-type'] = 'url';
-			/*$tempad['markup'] = (rand(1, 2) == 2 ?
-					'<html><body style="height:100%;background-color:green">this is a <b style="color:red">test</b> string</body></html>' :
-					'<html><body style="height:100%;background-color:green">HOORAYY INTERSTITIAL!</body></html>'
-			);*/
-			$tempad['markup'] = 'http://b.grabo.bg/mobile/_nimasystems/popup/?affid=18850';
-
-			/*<?xml version="1.0" encoding="UTF-8" ?>
-			 <ad type="interstitial" animation="None"><interstitial preload="0" autoclose="0" type="markup"  orientation="portrait"><markup><![CDATA[<html><body>HOORAYY!</body></html>]]></markup><skipbutton show="1" showafter="0"></skipbutton><navigation show="0"><topbar custombackgroundurl="" show="0" title="fixed" titlecontent=""></topbar><bottombar custombackgroundurl="" show="0" backbutton="0" forwardbutton="0" reloadbutton="0" externalbutton="0" timer="0"></bottombar></navigation></interstitial></ad>*/
-
-			// http://madserve.localhost/md.request.php?sdk=vad&c.mraid=0&o_iosadvidlimit=0&u=Mozilla%2F5.0%20%28iPhone%3B%20CPU%20iPhone%20OS%207_1%20like%20Mac%20OS%20X%29%20AppleWebKit%2F537.51.2%20%28KHTML%2C%20like%20Gecko%29%20Mobile%2F11D5145e&u_wv=Mozilla%2F5.0%20%28iPhone%3B%20CPU%20iPhone%20OS%207_1%20like%20Mac%20OS%20X%29%20AppleWebKit%2F537.51.2%20%28KHTML%2C%20like%20Gecko%29%20Mobile%2F11D5145e&u_br=Mozilla%2F5.0%20%28iPhone%3B%20CPU%20iPhone%20OS%207_1%20like%20Mac%20OS%20X%29%20AppleWebKit%2F537.51.2%20%28KHTML%2C%20like%20Gecko%29%20Version%2Funknown%20Mobile%2F11D5145e%20Safari%2Funknown&v=4.1.6&s=21080a259d6574c7863ca168b47e670a&iphone_osversion=7.1&o_iosadvid=92FCD837-4F8E-4B58-9158-30E390D08C26&rt=iphone_app&t=1391747751.608625&i=10.10.9.56
+			$tempad['markup'] = 'http://b.grabo.bg' . $http->path;
 		}
-
-		/*
-		 if (isset($xml_response->Ads->Ad['type'])) {
-		$tempad['type']=$xml_response->Ads->Ad['type'];
-		} else {$tempad['type']='';
-		}
-
-		if (isset($xml_response->Ads->Ad)) {
-		$tempad['markup']=$xml_response->Ads->Ad;
-		} else {$tempad['markup']='';
-		}
-
-		if (isset($xml_response->Ads->Ad->AdURL)) {
-		$tempad['url']=$xml_response->Ads->Ad->AdURL;
-		} else {$tempad['url']='';
-		}
-
-		if ($tempad['url']=='null' or $tempad['url']==''){
-		return false;
-		}*/
 
 		if ($request_type=='banner'){
 
