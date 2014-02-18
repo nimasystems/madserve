474a5abf42613f0c24cf539b67776122a16ab8d2
mkovachev <mkovachev@2f0e05dc-1968-4751-bacf-4d00df7d842d>
2014-02-18 09:50:38 +0000

    Changed: grabo changes

--- upload/modules/network_modules/GRABO/request.php
+++ upload/modules/network_modules/GRABO/request.php
@@ -6,7 +6,8 @@ class GRABO implements networkmodule {
 			
 		global $zone_detail;
 
-		$request_url = 'http://b.grabo.bg/mobile/_nimasystems/popup';
+		$request_url = $request_type == 'banner' ? 'http://b.grabo.bg/mobile/_nimasystems/footer' :
+		'http://b.grabo.bg/mobile/_nimasystems/popup';
 
 		$httpConfig['method']     = 'GET';
 		$httpConfig['timeout']    = 1;
@@ -20,8 +21,27 @@ class GRABO implements networkmodule {
 		$http->addParam('zone_refresh_time', $zone_detail['zone_refresh']);
 		$http->addParam('zone_lastrequest', $zone_detail['zone_lastrequest']);
 		$http->addParam('zone_placement_id', $request_info['placement_hash']);
-		
-		if ($request_type=='banner' or $request_type=='interstitial'){
+
+		if ($request_type=='banner' || $request_type=='interstitial'){
+
+			// temp
+			if ($request_type == 'interstitial') {
+				if ($request_info['main_device'] == 'IPAD') {
+					$zone_detail['zone_width'] = '768';
+					$zone_detail['zone_height'] = '1024';
+				} else {
+					$zone_detail['zone_width'] = '320';
+					$zone_detail['zone_height'] = '480';
+				}
+			} elseif ($request_type == 'banner') {
+				if ($request_info['main_device'] == 'IPAD') {
+					$zone_detail['zone_width'] = '600';
+					$zone_detail['zone_height'] = '50';
+				} else {
+					$zone_detail['zone_width'] = '320';
+					$zone_detail['zone_height'] = '50';
+				}
+			}
 
 			// private vars!
 			//$http->addParam('p_carrier'   , $request_info['ip_address']);
@@ -35,8 +55,8 @@ class GRABO implements networkmodule {
 			$http->addParam('p_main_device', $request_info['main_device']);
 			$http->addParam('p_device_os', $request_info['device_os']);
 			$http->addParam('p_iphone_osversion', $request_info['iphone_osversion']);
-			$http->addParam('p_adspace_width', $request_info['adspace_width']);
-			$http->addParam('p_adspace_height', $request_info['adspace_height']);
+			//$http->addParam('p_adspace_width', $request_info['adspace_width']);
+			//$http->addParam('p_adspace_height', $request_info['adspace_height']);
 			$http->addParam('p_channel', $request_info['channel']);
 			$http->addParam('p_geo_country', $request_info['geo_country']);
 			$http->addParam('p_geo_region', $request_info['geo_region']);
@@ -48,22 +68,46 @@ class GRABO implements networkmodule {
 		$http->execute($request_url);
 
 		if ($http->error){
-		return false;
+			return false;
 		}
 
 		if ($http->result=='' or preg_match("<!-- mKhoj: No advt for this position -->" , $http->result)){
-		return false;
+			return false;
 		}
 
-		if ($request_type == 'banner') {
+		$metas = array(
+				'apple-mobile-web-app-capable' => 'yes',
+				'apple-mobile-web-app-status-bar-style' => 'black',
+				'viewport' => 'initial-scale=1.0,user-scalable=no,minimum-scale=1.0,maximum-scale=1.0'
+		);
 
-			$tempad['url'] = 'http://b.grabo.bg/mobile/_nimasystems/popup/?affid=18850';
+		$m = array();
+
+		foreach($metas as $k => $v) {
+			$m[] = '<meta name="' . $k . '" content="' . $v . '" />';
+			unset($k, $v);
+		}
+
+		$m = implode("\n", $m);
+
+		$http->result = str_replace('<head>', '<head>' . "\n" . $m . "\n", $http->result);
+		$http->result = str_replace('href="//', 'href="http://', $http->result);
+
+		if ($request_type == 'banner') {
+			
+			$s = '320x480';
+			
+			if ($request_info['main_device'] == 'IPAD') { 
+				$s = '1024x768';
+			}
+			
+			$tempad['url'] = 'http://b.grabo.bg/mobile/_nimasystems/popup/?affid=18850&zone_size=' . $s;
 			$tempad['type'] = 'banner';
 			$tempad['markup'] = $http->result;
 		} else {
 			$tempad['type'] = 'banner';
-			$tempad['interstitial-type'] = 'url';
-			$tempad['markup'] = 'http://b.grabo.bg' . $http->path;
+			$tempad['interstitial-type'] = 'html';
+			$tempad['markup'] = $http->result; /*'http://b.grabo.bg' . $http->path;*/
 		}
 
 		if ($request_type=='banner'){
@@ -108,15 +152,15 @@ class GRABO implements networkmodule {
 				$ad=array();
 				$ad['main_type']='interstitial';
 				$ad['type']='interstitial';
-				$ad['animation']='None';
+				$ad['animation']='fade-in';
 				/* Interstitial */
 				$ad['interstitial-orientation']='portrait';
-				$ad['interstitial-preload']=0;
+				$ad['interstitial-preload']=1;
 				$ad['interstitial-autoclose']=0;
-				$ad['interstitial-type']=(isset($tempad['interstitial-type']) ? $tempad['interstitial-type'] : 'markup');
+				$ad['interstitial-type']='markup';
 				$ad['interstitial-content']=$tempad['markup'];
 				$ad['interstitial-skipbutton-show']=1;
-				$ad['interstitial-skipbutton-showafter']=0;
+				$ad['interstitial-skipbutton-showafter']=3;
 				$ad['interstitial-navigation-show']=0;
 				$ad['interstitial-navigation-topbar-show']=0;
 				$ad['interstitial-navigation-bottombar-show']=0;
@@ -126,8 +170,8 @@ class GRABO implements networkmodule {
 				$ad['interstitial-navigation-topbar-titlecontent']='';
 				$ad['interstitial-navigation-bottombar-backbutton']=0;
 				$ad['interstitial-navigation-bottombar-forwardbutton']=0;
-				$ad['interstitial-navigation-bottombar-reloadbutton']=0;
-				$ad['interstitial-navigation-bottombar-externalbutton']=0;
+				$ad['interstitial-navigation-bottombar-reloadbutton']=1;
+				$ad['interstitial-navigation-bottombar-externalbutton']=1;
 				$ad['interstitial-navigation-bottombar-timer']=0;
 				/* Interstitial */
 				return $ad;
