d2d441db3f05c7cb221d3b2365f0a563ab8c0539
mkovachev <mkovachev@2f0e05dc-1968-4751-bacf-4d00df7d842d>
2014-02-19 08:07:45 +0000

    Changed: some additions

---
+++ trunk/upload/cc.php
@@ -0,0 +1,31 @@
+<?php
+
+//setcookie('testc', '12345', time() + 1024 * 60 * 60, '/', '.madserve.dev.bgmiracle.com');
+
+
+$c = $_COOKIE;
+
+var_dump($c);
+
+//var_dump($_SERVER);
+
+
+?>
+<html>
+<head></head>
+<body>
+	<script type="text/javascript">
+	function setCookie(c_name,value,expiredays)
+	{
+	    var exdate=new Date();
+	    exdate.setDate(exdate.getDate()+expiredays);
+	    document.cookie=c_name+ "=" +escape(value)+
+	    ((expiredays==null) ? "" : ";expires="+exdate.toGMTString()+ "; path=/;"); 
+	    document.cookie="expiration"+ "=" +escape(exdate.toGMTString())+
+	    ((expiredays==null) ? "" : ";expires="+exdate.toGMTString()+ "; path=/;"); 
+	}
+setCookie('testc', 'RRRR', 123);
+alert('set');
+</script>
+</body>
+</html>
