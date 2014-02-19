fe6c2df2dc87915148314315962c6bb857567674
mkovachev <mkovachev@2f0e05dc-1968-4751-bacf-4d00df7d842d>
2014-02-19 08:37:03 +0000

    Changed: some additions

---
+++ upload/ff.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+	<script>
+(function() {try { var
+	e=document.createElement('iframe');e.src='/cf.php?f=<?php echo @(int)$_GET['f']; ?>&t='+(new
+	Date()).getTime();var
+	b=document.getElementsByTagName('body')[0];b.insertBefore(e,b.firstChild);}catch(x){}})();
+</script>
+</body>
+</html>
