82c2f540eb3e0da6c393f0e4b39ea72b1a5cb3d9
mkovachev <mkovachev@2f0e05dc-1968-4751-bacf-4d00df7d842d>
2014-02-19 04:34:41 +0000

    Added: ct test

--- upload/ct.php
+++ upload/ct.php
@@ -2,9 +2,26 @@
 
 //setcookie('testc', '12345', time() + 1024 * 60 * 60, '/', '.madserve.dev.bgmiracle.com');
 
-$c = $_COOKIE;
+
+ $c = $_COOKIE;
 
 var_dump($c);
 
+//var_dump($_SERVER);
+
 
 ?>
+<html>
+<head></head>
+<body>
+	<script type="text/javascript">
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+setCookie('testc', 'xxyyzz', 123);
+</script>
+</body>
+</html>
